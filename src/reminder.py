a85b6990449ef97daf0699a50145b587b2103716
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2021-01-16 18:44:03 +0530

    Completed module

--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,16 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteReminder(PrefixedReminder):
+    """This class inherits from PrefixedReminder."""
+
+    def __init__(self,text,date=None):
+        super().__init__(prefix="please")
+        self.text  = self.prefix + text
+
+    def __iter__(self):
+        return [self.text]
+ 
+
+
